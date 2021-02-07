9195c298a3c7a61b9d3c05e13fc7c3dcf9aa40b2
khassanboi <khassanboi@gmail.com>
2021-02-07 14:25:27 +0500

    S-Pinned
    Section of pinned news has been developed and responsiveness has been implemented

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
+    let toggleButton = () => {};
     $(".header__menu-open").click(() => {
-        $(".nav").css("transform", "translateX(0)");
+        $(".nav").css("top", "56px");
+        $(".header").css("box-shadow", "none");
         $(".header__menu-open").toggle();
         $(".header__menu-close").toggle();
     });
 
     $(".header__menu-close").click(() => {
-        $(".nav").css("transform", "translateX(100%)");
+        $(".nav").css("top", "-80%");
+        $(".header").css("box-shadow", "0 0 10px rgba(0, 0, 0, 0.2)");
         $(".header__menu-open").toggle();
         $(".header__menu-close").toggle();
     });
