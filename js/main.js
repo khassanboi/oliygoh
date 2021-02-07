cc18d174871c594ea73800b5e8b3df3618e3dfa8
khassanboi <khassanboi@gmail.com>
2021-02-07 15:15:39 +0500

    S-Stats
    Small section of summarized statistics has been developed and responsiveness has been implemented

--- js/main.js
+++ js/main.js
@@ -1,16 +1,29 @@
 $(document).ready(function () {
-    let toggleButton = () => {};
-    $(".header__menu-open").click(() => {
-        $(".nav").css("top", "56px");
-        $(".header").css("box-shadow", "none");
+    let toggleButtonAndShadow = () => {
         $(".header__menu-open").toggle();
         $(".header__menu-close").toggle();
+        $(".header").toggleClass("header--shaded");
+    };
+
+    $(".header__menu-open").click(() => {
+        $(".nav").css({
+            top: "7.6rem",
+            opacity: "1",
+            "pointer-events": "visible",
+            transition: "top 0.5s, opacity 0.001s",
+        });
+
+        toggleButtonAndShadow();
     });
 
     $(".header__menu-close").click(() => {
-        $(".nav").css("top", "-80%");
-        $(".header").css("box-shadow", "0 0 10px rgba(0, 0, 0, 0.2)");
-        $(".header__menu-open").toggle();
-        $(".header__menu-close").toggle();
+        $(".nav").css({
+            top: "-80%",
+            opacity: "0",
+            "pointer-events": "none",
+            transition: "top 0.5s, opacity 1s",
+        });
+
+        toggleButtonAndShadow();
     });
 });
